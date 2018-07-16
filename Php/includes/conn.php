e41a428fcf0bb24cd29e8449bcaf12061cbde8df
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-07-16 12:37:18 +0200

    connection

---
+++ Php/includes/conn.php
@@ -0,0 +1,17 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "shop";
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=shop", $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+	    }
+	catch(PDOException $e)
+	    {
+	    echo "Connection failed: " . $e->getMessage();
+	    }
+
+?>
