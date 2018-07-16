6c5acaa3eeb9af3dbb9a0c1cb1cad379e65ec865
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-07-16 12:35:23 +0200

    index

---
+++ Php/index.php
@@ -0,0 +1,31 @@
+
+<?php
+
+	include('includes/header.php');
+	
+?>
+
+<div class="content">
+	
+<?php
+
+	showMenu();
+
+	echo "<div class='products'>";
+
+	if (isset($_GET['cat_id'])) {
+		$category_id = $_GET['cat_id'];
+	}
+	else{
+		$category_id = null;
+	}
+	showCategory($category_id);
+	echo "</div>";
+	
+?>
+</div>
+<?php 
+
+	include('includes/footer.php');
+
+?>
