09b203b10a734ff0e176c62b1464cd472716377f
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-07-22 10:55:23 +0200

    login forms

---
+++ Php/includes/login.php
@@ -0,0 +1,11 @@
+<form action="doLogin.php" method="POST" >
+	<input type="text" name="login" placeholder="Login" required>
+	<input type="password" name="password" placeholder="Password" required>
+	<input type="submit"value="Log in">
+</form>
+<hr>
+<h3 id="new">Are you new ?</h3>
+<a href="signUp.php">
+	<input type="button" value="Sign up">
+</a>
+
