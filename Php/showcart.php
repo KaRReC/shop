4abf364f5b3e1cc0815ba7560cd4da0748d7b744
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-07-22 11:01:14 +0200

    Update showcart.php

--- Php/showcart.php
+++ Php/showcart.php
@@ -8,27 +8,42 @@
 
 	echo "<div class='products'>";
 
+	echo "<h2>Content of your shopping cart:</h2>";
+
 	echo "<table>";
 
 	$inCart = $cart->getProducts();
 
-	echo "<tr><td>Index</td><td>Name</td><td>Price</td><td>Quantity</td><td>Total price</td></tr>";
+	echo "<tr><th>Index</th><th>Name</th><th>Price</th><th>Quantity</th><th>Total price</th></tr>";
 
 	$sum = 0;
 	foreach ($inCart as $product) {
+		$productCartId = $product['id'];
 		$price = $product['price'];
 		$quantity = $product['quantity'];
 		$Pindex = $product['Pindex'];
 		$name = $product['name'];
 		$total = $quantity * $price;
+		$id = $product['pid'];
 		$sum+= $total;
-		echo "<tr><td>$Pindex</td><td>$name</td><td>$price</td><td>$quantity</td><td>$total &#x20AC</td></tr>";
+
+		$remove = "<a href='remFromCart.php?id=$productCartId'>Remove</a>";
+		$plus = "<a id='plus' href='addToCart.php?id=$id'> + </a>";
+		$minus = "<a id='minus' href='remFromCart.php?id=$id'> - </a>";
+
+		echo "<tr><td>$Pindex</td><td>$name</td><td>$price &#x20AC </td><td> $minus $quantity $plus </b></h1></td><td>$total &#x20AC</td></tr>";
+
 
 	}
-
+	echo "<tr><td></td><td></td><td></td><td></td><td><h3>$sum &#x20AC</h3></td></tr>";
 	echo "</table>";
 
-	echo "<h3 id='sum'>".$sum." &#x20AC</h3>";
+	if ($cart->getProducts()) {
+		echo "<div class='order'>";
+		echo "<a href='order.php'><input type='submit' value='Order'></a>";
+		echo "</div>";
+		
+	}
 
 	echo "</div></div>";
 
