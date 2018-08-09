eaee243c129e4e87910e0dc55944e16cd04af5da
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-08-09 17:45:35 +0200

    Update login.php

--- Php/includes/login.php
+++ Php/includes/login.php
@@ -1,7 +1,7 @@
 <form action="doLogin.php" method="POST" >
 	<input type="text" name="login" placeholder="Login" required>
 	<input type="password" name="password" placeholder="Password" required>
-	<input type="submit"value="Log in">
+	<input type="submit" value="Log in">
 </form>
 <hr>
 <h3 id="new">Are you new ?</h3>
